ca4b48fe18da9c3fe486363b5006cc951d1a773a
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-04-25 14:55:07 +0530

    Add responsive sidebar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,16 +1,50 @@
-import SideBar from "@/components/sidebar";
+"use client";
 
-export default function Layout({
-    children,
-}: {
-    children: React.ReactNode
-}) {
-    return (
-        <div className="flex h-screen items-stretch">
-            <SideBar />
-            <div className="flex-1 bg-gray-100 overflow-auto">
-                {children}
-            </div>
+import SideBar from "@/components/sidebar";
+import { useState } from "react";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  const [isSidebarExpanded, setSidebarExpanded] = useState(false);
+
+  function toggleSidebar() {
+    setSidebarExpanded(!isSidebarExpanded);
+  }
+  return (
+    <div className="inset-0 bg-gray-100">
+      <button
+        onClick={toggleSidebar}
+        type="button"
+        className={`${
+          isSidebarExpanded ? "hidden" : "block"
+        } absolute h-12 md:hidden items-center ml-3 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-200 dark:focus:ring-gray-200`}
+      >
+        <span className="sr-only">Open sidebar</span>
+        <div className="space-y-1">
+          <span className="block w-6 h-0.5 bg-gray-600"></span>
+          <span className="block w-6 h-0.5 bg-gray-600"></span>
+          <span className="block w-4 h-0.5 bg-gray-600"></span>
         </div>
-    )
+      </button>
+      <div className="inset-0 flex">
+        <div
+          className={`${
+            isSidebarExpanded
+              ? "translate-x-0 absolute"
+              : "-translate-x-full absolute"
+          } md:translate-x-0 md:contents items-stretch z-40 md:z-0 duration-200 ease-in-out h-screen`}
+        >
+          <SideBar />
+        </div>
+
+        <div
+          className={`overflow-auto w-full h-screen flex-1`}
+          onClick={() => {
+            if (isSidebarExpanded) setSidebarExpanded(false);
+          }}
+        >
+          {children}
+        </div>
+      </div>
+    </div>
+  );
 }
