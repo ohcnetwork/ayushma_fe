2880c80ee87a8eeb31f8f4f9df7c6ec373e0a598
skks1212 <kacker.shivank@gmail.com>
2023-05-05 01:05:33 +0530

    recording indicator

--- src/components/chatbar.tsx
+++ src/components/chatbar.tsx
@@ -20,26 +20,37 @@ export default function ChatBar(props: {
     });
 
     return (
-        <form onSubmit={onSubmit}>
-            <Input
-                type="text"
-                placeholder="Chat"
-                value={chat || ""}
-                onChange={onChange}
-                loading={loading}
-                errors={errors}
-                right={loading ?
-                    <Loading /> :
-                    <>
-                        <button type="button" className={`px-4 ${status === "recording" ? "text-green-500" : ""}`} onClick={status === "recording" ? stopRecording : startRecording}>
-                            <i className="fal fa-microphone" />
-                        </button>
-                        <button className="px-4 disabled:text-gray-300" disabled={chat.length < 1}>
-                            <i className="fal fa-paper-plane-top" />
-                        </button>
-                    </>
-                }
-            />
-        </form>
+        <>
+            <div className={`fixed inset-0 flex items-center justify-center transition-all ${status === "recording" ? "visible opacity-100" : "invisible opacity-0"}`}>
+                <div className="bg-black/40 absolute inset-0 -z-10" />
+                <div className="bg-white rounded-xl p-4 flex items-center flex-col gap-4">
+                    Listening...
+                    <button className="flex w-28 h-28 items-center justify-center text-4xl bg-red-500 rounded-full text-white hover:bg-red-600" onClick={stopRecording}>
+                        <i className="fas fa-microphone-slash" />
+                    </button>
+                </div>
+            </div>
+            <form onSubmit={onSubmit}>
+                <Input
+                    type="text"
+                    placeholder="Chat"
+                    value={chat || ""}
+                    onChange={onChange}
+                    loading={loading}
+                    errors={errors}
+                    right={loading ?
+                        <Loading /> :
+                        <>
+                            <button type="button" className={`px-4 ${status === "recording" ? "text-green-500" : ""}`} onClick={status === "recording" ? stopRecording : startRecording}>
+                                <i className="fal fa-microphone" />
+                            </button>
+                            <button className="px-4 disabled:text-gray-300" disabled={chat.length < 1}>
+                                <i className="fal fa-paper-plane-top" />
+                            </button>
+                        </>
+                    }
+                />
+            </form>
+        </>
     )
 }
