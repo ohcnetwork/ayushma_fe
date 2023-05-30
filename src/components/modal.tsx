9b2c90fbdf52598251a9af82c922bc2488b997c8
skks1212 <kacker.shivank@gmail.com>
2023-05-30 23:37:58 +0530

    updated modal animation

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -9,7 +9,7 @@ export default function Modal(props: {
     return (
         <div className={`inset-0 fixed z-10 flex items-center justify-center transition-all ${show ? "opacity-100 visible" : "opacity-0 invisible"}`}>
             <div onClick={onClose} className={`bg-black/40 absolute inset-0 -z-10`} />
-            <div className={`bg-white md:rounded-xl w-full md:w-[800px] h-full md:h-[500px] p-8 relative transition-all ${show ? "scale-100" : "scale-[0.2]"} overflow-auto`}>
+            <div className={`bg-white md:rounded-xl w-full md:w-[800px] h-full md:h-[500px] p-8 relative transition-all ${show ? "scale-100 opacity-100" : "scale-[0.8] opacity-0"} overflow-auto`}>
                 <button onClick={onClose} className="absolute top-4 right-6 text-xl">
                     <i className="far fa-times" />
                 </button>
