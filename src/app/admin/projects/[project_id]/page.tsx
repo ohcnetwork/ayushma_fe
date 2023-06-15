beed309ecfca12c621ce9051ab7bd29b6982f85e
Suprabath <34211797+suprabathk@users.noreply.github.com>
2023-06-15 15:23:39 +0530

    Added indicator for document type (#36)

--- src/app/admin/projects/[project_id]/page.tsx
+++ src/app/admin/projects/[project_id]/page.tsx
@@ -9,65 +9,93 @@ import Link from "next/link";
 import { useRouter } from "next/navigation";
 
 export default function Page({ params }: { params: { project_id: string } }) {
+  const { project_id } = params;
+  const projectsQuery = useQuery(["project", project_id], () =>
+    API.projects.get(project_id)
+  );
+  const project: Project | undefined = projectsQuery.data || undefined;
+  const documentsQuery = useQuery(["project", project_id, "documents"], () =>
+    API.documents.list(project_id)
+  );
+  const documents: Document[] | undefined = documentsQuery.data?.results;
 
-    const { project_id } = params;
-    const projectsQuery = useQuery(["project", project_id], () => API.projects.get(project_id));
-    const project: Project | undefined = projectsQuery.data || undefined;
-    const documentsQuery = useQuery(["project", project_id, "documents"], () => API.documents.list(project_id));
-    const documents: Document[] | undefined = documentsQuery.data?.results;
+  const docIconsClassNames = {
+    1: "fa-file-text-o",
+    2: "fa-link",
+    3: "fa-font",
+  };
 
-    const router = useRouter();
+  const router = useRouter();
 
-    const updateProjectMutation = useMutation((project: Partial<Project>) => API.projects.update(project_id, project), {
-        onSuccess: () => {
-            projectsQuery.refetch();
-        }
-    });
-
-    const deleteProjectMutation = useMutation(() => API.projects.delete(project_id), {
-        onSuccess: () => {
-            router.push("/admin");
-        }
-    });
-
-    const handleProjectSave = async (project: Partial<Project>) => {
-        await updateProjectMutation.mutateAsync(project);
+  const updateProjectMutation = useMutation(
+    (project: Partial<Project>) => API.projects.update(project_id, project),
+    {
+      onSuccess: () => {
+        projectsQuery.refetch();
+      },
     }
+  );
 
-    const handleDelete = async () => {
-        if (confirm("Are you sure you want to delete this project?")) {
-            await deleteProjectMutation.mutateAsync();
-        }
+  const deleteProjectMutation = useMutation(
+    () => API.projects.delete(project_id),
+    {
+      onSuccess: () => {
+        router.push("/admin");
+      },
     }
+  );
 
-    return (
-        <div>
-            <h1 className="text-3xl font-black">
-                {project?.title}
-            </h1>
-            <h2 className="text-2xl mt-6 font-bold mb-4">
-                Documents
-            </h2>
-            <div className="grid grid-cols-4 gap-4 mt-8">
-                {documents?.map((document, i) => (
-                    <Link href={`/admin/projects/${project_id}/documents/${document.external_id}`} key={i} className="border border-gray-300 hover:bg-gray-100 rounded-lg p-4">
-                        {document.title}
-                    </Link>
-                ))}
-                <Link href={`/admin/projects/${project_id}/documents/new`} className="border border-dashed border-gray-300 hover:bg-gray-100 rounded-lg p-4">
-                    <i className="far fa-plus" /> New Document
-                </Link>
-            </div>
-            <h2 className="text-2xl mt-6 font-bold mb-4">
-                Details
-            </h2>
-            {project && <ProjectForm project={project} onSubmit={handleProjectSave} loading={updateProjectMutation.isLoading} errors={updateProjectMutation.error} />}
-            <Button
-                className="bg-red-500 hover:bg-red-600 mt-8"
-                onClick={handleDelete}
-            >
-                Delete Project
-            </Button>
-        </div>
-    )
+  const handleProjectSave = async (project: Partial<Project>) => {
+    await updateProjectMutation.mutateAsync(project);
+  };
+
+  const handleDelete = async () => {
+    if (confirm("Are you sure you want to delete this project?")) {
+      await deleteProjectMutation.mutateAsync();
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="text-3xl font-black">{project?.title}</h1>
+      <h2 className="text-2xl mt-6 font-bold mb-4">Documents</h2>
+      <div className="grid grid-cols-4 gap-4 mt-8">
+        {documents?.map((document, i) => (
+          <Link
+            href={`/admin/projects/${project_id}/documents/${document.external_id}`}
+            key={i}
+            className="border border-gray-300 hover:bg-gray-100 rounded-lg p-4 flex items-center gap-2"
+          >
+            <i
+              className={
+                "text-gray-800 fa " + docIconsClassNames[document.document_type]
+              }
+            />
+            {document.title}
+          </Link>
+        ))}
+        <Link
+          href={`/admin/projects/${project_id}/documents/new`}
+          className="border border-dashed border-gray-300 hover:bg-gray-100 rounded-lg p-4"
+        >
+          <i className="far fa-plus" /> New Document
+        </Link>
+      </div>
+      <h2 className="text-2xl mt-6 font-bold mb-4">Details</h2>
+      {project && (
+        <ProjectForm
+          project={project}
+          onSubmit={handleProjectSave}
+          loading={updateProjectMutation.isLoading}
+          errors={updateProjectMutation.error}
+        />
+      )}
+      <Button
+        className="bg-red-500 hover:bg-red-600 mt-8"
+        onClick={handleDelete}
+      >
+        Delete Project
+      </Button>
+    </div>
+  );
 }
