affcbab21b8829a91ae406efde83de8b1eca3821
suprabathk <suprabathkondapally@gmail.com>
2023-06-19 19:22:50 +0530

    Added proper layout to document form

--- src/components/forms/documentform.tsx
+++ src/components/forms/documentform.tsx
@@ -2,146 +2,213 @@ import { Document, DocumentType, Project } from "@/types/project";
 import { useEffect, useState } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { API } from "@/utils/api";
-import { Button, Errors, Input, TextArea } from "../ui/interactive";
+import { Button, Dropdown, Errors, Input, TextArea } from "../ui/interactive";
 import { API_BASE_URL } from "@/utils/api";
 import { useRouter } from "next/navigation";
 
 export default function DocumentForm(props: {
-    document: Partial<Document>,
-    project_id: string,
-    onSubmit: (document: Partial<Document>) => void,
-    errors?: any
-    loading?: boolean
+  document: Partial<Document>;
+  project_id: string;
+  onSubmit: (document: Partial<Document>) => void;
+  errors?: any;
+  loading?: boolean;
 }) {
+  const { document: doc, onSubmit, errors, loading, project_id } = props;
+  const [document, setDocument] = useState<Partial<Document>>(doc);
 
-    const { document: doc, onSubmit, errors, loading, project_id } = props;
-    const [document, setDocument] = useState<Partial<Document>>(doc);
+  const router = useRouter();
 
-    const router = useRouter();
+  const docIconsClassNames = {
+    1: "file-text-o",
+    2: "link",
+    3: "font",
+  };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        onSubmit(document);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(document);
+  };
+
+  const deleteDocumentMutation = useMutation(
+    () => API.documents.delete(project_id, doc.external_id ?? ""),
+    {
+      onSuccess: () => {
+        router.push(`/admin/projects/${project_id}`);
+      },
     }
+  );
 
-    const deleteDocumentMutation = useMutation(() => API.documents.delete(project_id, doc.external_id ?? ""), {
-        onSuccess: () => {
-            router.push(`/admin/projects/${project_id}`);
-        }
-    });
-
-    const handleDelete = async () => {
-        if (confirm("Are you sure you want to delete this document?")) {
-            await deleteDocumentMutation.mutateAsync();
-        }
+  const handleDelete = async () => {
+    if (confirm("Are you sure you want to delete this document?")) {
+      await deleteDocumentMutation.mutateAsync();
     }
+  };
 
-    useEffect(() => {
-        console.log(errors)
-    }, [errors])
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
 
-    return (
-        <div>
-            <Errors errors={errors?.non_field_errors} />
-            <form onSubmit={handleSubmit} className="flex gap-2" encType="multipart/form-data">
-                
-                <div className="flex-1">
-                    {doc.file ? (
-                        <div>
-                            <iframe
-                                src={doc.file as any}
-                                className="w-full h-64"
-                            />
-                        </div>
-                    ) : (doc.text_content ? (
-                        <div>
-                            {doc.text_content}
-                        </div>
-                    ) : (
-                        <>
-                            {document.document_type === DocumentType.FILE && (
-                                <>
-                                    <input
-                                        type="file"
-                                        accept=".pdf,.txt"
-                                        onChange={(e) => {
-                                            const file = e.target.files?.[0];
-                                            if (file) {
-                                                setDocument({ ...document, file });
-                                            }
-                                        }}
-                                    />
-                                    <Errors errors={errors?.file} />
-                                    <Errors errors={errors?.detail} />
-                                </>
-                            )}
-                            {document.document_type === DocumentType.TEXT && (
-                                <>
-                                    <TextArea
-                                        placeholder="Text Content"
-                                        value={document.text_content}
-                                        onChange={(e) => setDocument({ ...document, text_content: e.target.value })}
-                                        errors={errors?.text_content}
-                                    />
-                                    <Errors errors={errors?.detail} />
-                                </>
-                            )}
-                            {document.document_type === DocumentType.URL && (
-                                <>
-                                    <Input
-                                        placeholder="URL"
-                                        value={document.text_content}
-                                        onChange={(e) => setDocument({ ...document, text_content: e.target.value })}
-                                        errors={errors?.text_content}
-                                    />
-                                    <Errors errors={errors?.detail} />
-                                </>
-                            )}
-                        </>
-                    ))
-                    }
-                </div>
-                <div className="flex flex-col gap-2 w-96">
-                    Document Type
-                    <select
-                        value={document.document_type || 0}
-                        onChange={(e) => setDocument({ ...document, document_type: parseInt(e.target.value) })}
-                    >
-                        <option value={0} disabled>SELECT</option>
-                        <option value={DocumentType.FILE}>File</option>
-                        <option value={DocumentType.URL}>URL</option>
-                        <option value={DocumentType.TEXT}>TEXT</option>
-                    </select>
-                    <Input
-                        placeholder="Title"
-                        value={document.title}
-                        onChange={(e) => setDocument({ ...document, title: e.target.value })}
-                        errors={errors?.title}
-                    />
-                    <TextArea
-                        placeholder="Description"
-                        value={document.description}
-                        onChange={(e) => setDocument({ ...document, description: e.target.value })}
-                        errors={errors?.description}
-                    />
-                    <Button
-                        loading={loading}
-                        type="submit"
-                    >
-                        {doc.external_id ? "Save" : "Create"}
-                    </Button>
-
-                    {doc.external_id && (
-                        <Button
-                            className="bg-red-500 hover:bg-red-600 mt-8"
-                            onClick={handleDelete}
-                            type="button"
-                        >
-                            Delete Document
-                        </Button>
-                    )}
-                </div>
-            </form>
+  return (
+    <div>
+      <Errors errors={errors?.non_field_errors} />
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col-reverse sm:flex-row gap-2"
+        encType="multipart/form-data"
+      >
+        <div className="flex flex-col gap-2 sm:w-96 ">
+          {!doc.external_id && (
+            <Dropdown
+              value={document.document_type || 0}
+              onChange={(e) =>
+                setDocument({
+                  ...document,
+                  document_type: parseInt(e.target.value),
+                })
+              }
+            >
+              <option value={0} disabled>
+                SELECT
+              </option>
+              <option value={DocumentType.FILE}>File</option>
+              <option value={DocumentType.URL}>URL</option>
+              <option value={DocumentType.TEXT}>Text</option>
+            </Dropdown>
+          )}
+          <Input
+            placeholder="Title"
+            value={document.title}
+            onChange={(e) =>
+              setDocument({ ...document, title: e.target.value })
+            }
+            errors={errors?.title}
+          />
+          <TextArea
+            placeholder="Description"
+            value={document.description}
+            className="h-full"
+            onChange={(e) =>
+              setDocument({ ...document, description: e.target.value })
+            }
+            errors={errors?.description}
+          />
+          <Button loading={loading} type="submit">
+            {doc.external_id ? "Save" : "Create"}
+          </Button>
+          {doc.external_id && (
+            <Button
+              className="bg-red-500 hover:bg-red-600 "
+              onClick={handleDelete}
+              type="button"
+            >
+              Delete Document
+            </Button>
+          )}
         </div>
-    )
+        <span className="border-l" />
+        <div className="flex-1 w-full">
+          {doc.file ? (
+            <div>
+              <iframe src={doc.file as any} className="w-full h-64" />
+            </div>
+          ) : doc.text_content ? (
+            <div className="flex flex-col gap-2 bg-[#fbfffd] h-full border border-gray-200 rounded-md p-4 ">
+              {doc.document_type && (
+                <div className="flex gap-2 items-center">
+                  <i
+                    className={`fa fa-${docIconsClassNames[doc.document_type]}`}
+                  />
+                  <span>{DocumentType[doc.document_type]}</span>
+                </div>
+              )}
+              <p>{doc.text_content}</p>
+            </div>
+          ) : (
+            <>
+              {document.document_type === DocumentType.FILE && (
+                <>
+                  <div className="bg-[#f4fef8] rounded-md border-2 border-green-500 h-full">
+                    <label
+                      htmlFor="file-picker"
+                      className="text-green-600 text-center flex gap-2 items-center justify-center h-full cursor-pointer outline-none"
+                    >
+                      {document.file ? (
+                        <p className="text-2xl font-semibold">
+                          {document.file?.name}
+                        </p>
+                      ) : (
+                        <div className="flex gap-2 items-center">
+                          <i className="fa fa-upload" aria-hidden="true" />
+                          <span className="text-xl font-medium">
+                            Upload a file
+                          </span>
+                        </div>
+                      )}
+                    </label>
+                    <input
+                      id="file-picker"
+                      hidden={true}
+                      type="file"
+                      accept=".pdf,.txt"
+                      onChange={(e) => {
+                        const file = e.target.files?.[0];
+                        if (file) {
+                          setDocument({ ...document, file });
+                        }
+                      }}
+                    />
+                    <Errors errors={errors?.file} />
+                    <Errors errors={errors?.detail} />
+                  </div>
+                </>
+              )}
+              {document.document_type === DocumentType.TEXT && (
+                <>
+                  <div className="ml-2 flex gap-2 items-center">
+                    <i className={`fa fa-font`} />
+                    <span>TEXT</span>
+                  </div>
+                  <TextArea
+                    placeholder="Please enter the text"
+                    className="bg-[#fbfffd] rounded-md text-black"
+                    value={document.text_content}
+                    onChange={(e) =>
+                      setDocument({ ...document, text_content: e.target.value })
+                    }
+                    errors={errors?.text_content}
+                  />
+                  <Errors errors={errors?.detail} />
+                </>
+              )}
+              {document.document_type === DocumentType.URL && (
+                <>
+                  <div className="ml-2 flex gap-2 items-center">
+                    <i className={`fa fa-link`} />
+                    <span>URL</span>
+                  </div>
+                  <Input
+                    placeholder="Please enter the URL"
+                    className="bg-[#fbfffd] rounded-md text-black"
+                    value={document.text_content}
+                    onChange={(e) =>
+                      setDocument({ ...document, text_content: e.target.value })
+                    }
+                    errors={errors?.text_content}
+                  />
+                  <Errors errors={errors?.detail} />
+                </>
+              )}
+              {!document.document_type && (
+                <div className="bg-[#fbfffd] border-2 h-full flex justify-center items-center border-gray-300 rounded-md text-gray-500">
+                  <span className="text-xl font-medium">
+                    Select a document type
+                  </span>
+                </div>
+              )}
+            </>
+          )}
+        </div>
+      </form>
+    </div>
+  );
 }
