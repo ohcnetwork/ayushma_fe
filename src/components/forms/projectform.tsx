0d70201fc8ac7057d9c9c6eba375ab1e20695d9c
Yogesh Bhutkar <pseudonetwork1001@gmail.com>
2023-06-20 18:53:34 +0530

    Added height fix

--- src/components/forms/projectform.tsx
+++ src/components/forms/projectform.tsx
@@ -3,48 +3,51 @@ import { useState } from "react";
 import { Button, Input, TextArea } from "../ui/interactive";
 
 export default function ProjectForm(props: {
-    project: Partial<Project>,
-    onSubmit: (project: Partial<Project>) => void,
-    errors?: any
-    loading?: boolean
+  project: Partial<Project>;
+  onSubmit: (project: Partial<Project>) => void;
+  errors?: any;
+  loading?: boolean;
 }) {
+  const { project: pro, onSubmit, errors, loading } = props;
+  const [project, setProject] = useState<Partial<Project>>(pro);
 
-    const { project: pro, onSubmit, errors, loading } = props;
-    const [project, setProject] = useState<Partial<Project>>(pro);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    onSubmit(project);
+  };
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        onSubmit(project);
-    }
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit} className="flex flex-col gap-2" encType="multipart/form-data">
-                <Input
-                    placeholder="Title"
-                    value={project.title}
-                    onChange={(e) => setProject({ ...project, title: e.target.value })}
-                    errors={errors?.title}
-                />
-                <TextArea
-                    placeholder="Description"
-                    value={project.description}
-                    onChange={(e) => setProject({ ...project, description: e.target.value })}
-                    errors={errors?.description}
-                />
-                <TextArea
-                    placeholder="Prompt"
-                    value={project.prompt}
-                    onChange={(e) => setProject({ ...project, prompt: e.target.value })}
-                    errors={errors?.prompt}
-                />
-                <Button
-                    loading={loading}
-                    type="submit"
-                >
-                    {pro.external_id ? "Save" : "Create"}
-                </Button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col gap-2"
+        encType="multipart/form-data"
+      >
+        <Input
+          placeholder="Title"
+          value={project.title}
+          onChange={(e) => setProject({ ...project, title: e.target.value })}
+          errors={errors?.title}
+        />
+        <TextArea
+          placeholder="Description"
+          value={project.description}
+          onChange={(e) =>
+            setProject({ ...project, description: e.target.value })
+          }
+          errors={errors?.description}
+        />
+        <TextArea
+          placeholder="Prompt"
+          className="h-56"
+          value={project.prompt}
+          onChange={(e) => setProject({ ...project, prompt: e.target.value })}
+          errors={errors?.prompt}
+        />
+        <Button loading={loading} type="submit">
+          {pro.external_id ? "Save" : "Create"}
+        </Button>
+      </form>
+    </div>
+  );
 }
