2675e92389ef431bdd032bec1d23e0d20baacbe9
Ashesh <3626859+Ashesh3@users.noreply.github.com>
2023-06-21 16:03:26 +0530

    Add STT engine option to ProjectForm (#47)
    * Add STT engine option to ProjectForm
    
    * Move stt engines to integer

--- src/types/project.ts
+++ src/types/project.ts
@@ -5,6 +5,7 @@ export type Project = BaseModelType & {
     description: string,
     is_default: boolean,
     prompt?: string,
+    stt_engine: number,
 }
 
 export enum DocumentType {
@@ -12,6 +13,11 @@ export enum DocumentType {
     URL = 2,
     TEXT = 3
 }
+
+export const STT_ENGINES = [
+    { id: 1, label: 'OpenAI Whisper' },
+    { id: 2, label: 'Google Speech to Text' },
+]
 
 export type Document = BaseModelType & {
     title: string,
