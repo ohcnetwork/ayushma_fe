ea2fd039d33846713f725929749cfa73842a7bfe
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-09-30 20:01:06 +0530

    show user in chat list

--- src/app/admin/projects/[project_id]/chats/page.tsx
+++ src/app/admin/projects/[project_id]/chats/page.tsx
@@ -24,7 +24,7 @@ export default function Page({ params }: { params: { project_id: string } }) {
       <h2 className="text-2xl mt-6 font-bold mb-4">Chats</h2>
       <div>
         {chats && chats.length > 0 ? (
-          <div className="grid grid-cols-4 gap-4 mt-8">
+          <div className="grid md:grid-cols-4 gap-4 mt-8">
             {chats.map((chat: Chat, i: number) => (
               <Link
                 href={`/admin/projects/${project_id}/chats/${chat.external_id}`}
@@ -32,6 +32,10 @@ export default function Page({ params }: { params: { project_id: string } }) {
                 className="border border-gray-300 hover:bg-gray-100 bg-white rounded-lg p-4"
               >
                 {chat.title}
+                <div className="text-sm flex gap-2 items-center mt-2 text-gray-600">
+                  <i className="fa fa-user"></i>
+                  {chat?.username}
+                </div>
               </Link>
             ))}
           </div>
