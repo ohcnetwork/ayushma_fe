5c2f19733c2bdbd6c48eec210e2c73feef19d27f
Pranshu Aggarwal <pranshu1902@gmail.com>
2023-09-30 16:50:56 +0530

    show username in each chat for admin

--- src/app/admin/projects/[project_id]/chats/[chat_id]/page.tsx
+++ src/app/admin/projects/[project_id]/chats/[chat_id]/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Chat } from "@/types/chat";
 import { Project } from "@/types/project";
 import { API } from "@/utils/api";
 import { useQuery } from "@tanstack/react-query";
@@ -26,30 +25,41 @@ export default function Page({
   return (
     <div>
       <h1 className="text-3xl font-bold">{project?.title}</h1>
-      <h2 className="text-2xl mt-6 font-bold mb-4">Chat: {chats?.title}</h2>
+      <div className="flex flex-col mt-6 md:flex-row justify-between items-center gap-2 mb-4">
+        <h2 className="text-2xl font-bold">Chat: {chats?.title}</h2>
+        <div className="flex justify-center items-center">
+          {chats?.username && (
+            <Link
+              href={`/admin/users/${chats?.username}`}
+              className="bg-slate-200 flex items-center gap-2 justify-center hover:bg-slate-300 m-2 p-2 rounded-xl px-6"
+            >
+              <i className="fa fa-user"></i>
+              {chats?.username}
+            </Link>
+          )}
+        </div>
+      </div>
       <div>
         <div className="flex flex-col gap-4 mt-8">
-          {chats &&
-            chats?.chats?.length > 0 ?
+          {chats && chats?.chats?.length > 0 ? (
             chats?.chats.map((chat: any, i: number) => (
               <div key={i}>
                 {chat.messageType === 1 ? (
-                  <div
-                    className="border border-gray-300 hover:bg-gray-100 bg-white rounded-lg p-4"
-                  >
+                  <div className="border border-gray-300 hover:bg-gray-100 bg-white rounded-lg p-4">
                     {chat.message}
                   </div>
                 ) : (
-                  <div
-                    className="border border-gray-300 hover:bg-gray-100 bg-gray-200 rounded-lg p-4"
-                  >
+                  <div className="border border-gray-300 hover:bg-gray-100 bg-gray-200 rounded-lg p-4">
                     {chat.message}
                   </div>
                 )}
               </div>
-            )) : (
-                <div className="flex justify-center bg-white rounded-xl p-3">No Chats Found</div>
-            )}
+            ))
+          ) : (
+            <div className="flex justify-center bg-white rounded-xl p-3">
+              No Chats Found
+            </div>
+          )}
         </div>
       </div>
     </div>
