ceb3df3885e1e763fed1512208b3af63a4f1db55
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-12-07 12:11:22 +0530

    Make pages responsive for mobile screens

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -49,7 +49,7 @@ export default function Page() {
         </button>
       </div>
       <div>
-        <div className="grid grid-cols-4 gap-4 mt-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
           <Link
             href={`/admin/projects/new`}
             className="border border-dashed border-gray-300 hover:bg-gray-100 bg-white rounded-lg p-4"
