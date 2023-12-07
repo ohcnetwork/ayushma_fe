1dcce5a2a8a1285bfb3ab7741dd3c52d25c6e96f
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-12-07 12:14:48 +0530

    modal width fix

--- src/components/modal.tsx
+++ src/components/modal.tsx
@@ -10,7 +10,7 @@ export default function Modal(props: {
     const { show, onClose, children, className } = props;
 
     return (
-        <div className={`absolute w-full p-2 inset-0 z-10 flex items-center justify-center transition-all ${show ? "opacity-100 visible" : "opacity-0 invisible"}`}>
+        <div className={`absolute w-screen p-2 inset-0 z-10 flex items-center justify-center transition-all ${show ? "block" : "hidden"}`}>
             <div onClick={onClose} className={`bg-black/40 absolute inset-0 -z-10`} />
             <div className={twMerge(`bg-white md:rounded-xl w-fit h-fit p-8 relative transition-all ${show ? "scale-100 opacity-100" : "scale-[0.8] opacity-0"} overflow-auto`, className)}>
                 <button onClick={onClose} className="absolute top-4 right-6 text-xl text-gray-400 hover:text-black ">
