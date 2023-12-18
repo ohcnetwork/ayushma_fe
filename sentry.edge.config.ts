bb0fef585c4cb16f1f31cc52ca34fd2757bc59b6
Ashesh3 <3626859+Ashesh3@users.noreply.github.com>
2023-12-18 16:28:03 +0530

    Update Sentry configuration for production environment

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -5,12 +5,14 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-  dsn: "https://90c2e47b6be68faea4ef064cfc08313e@o371117.ingest.sentry.io/4506415628288000",
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn: "https://90c2e47b6be68faea4ef064cfc08313e@o371117.ingest.sentry.io/4506415628288000",
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
+  });
+}
