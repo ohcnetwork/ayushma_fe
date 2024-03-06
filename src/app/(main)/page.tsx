46ee7b5835b2af34dd85bf7ffe21e0b6abda919d
Shivank Kacker <kacker.shivank@gmail.com>
2024-03-07 02:24:06 +0530

    test

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -4,20 +4,20 @@ import { redirect } from "next/navigation";
 import Client from "./client";
 
 const getDefaultProject = async () => {
-  const projects = await API.projects.list();
-  return projects.results.find((project: Project) => project.is_default);
+  //const projects = await API.projects.list();
+  //return projects.results.find((project: Project) => project.is_default);
 }
 
 export default async function Page() {
   let defaultProject;
   try {
-    defaultProject = await getDefaultProject();
+    //defaultProject = await getDefaultProject();
 
   } catch (error) {
     console.log(error);
   }
 
-  if (defaultProject) redirect(`/project/${defaultProject.external_id}`);
+  //if (defaultProject) redirect(`/project/${defaultProject.external_id}`);
 
   return <Client />
 }
